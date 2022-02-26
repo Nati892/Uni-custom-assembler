dafc6e335c4448abd4b80699d07d57c1d9a503f8
Nati892 <netanel892@gmail.com>
2022-02-26 09:08:50 +0200

    first label handler save

--- Label.h
+++ Label.h
@@ -6,28 +6,35 @@
 #include <string.h>
 #include <ctype.h>
 #include "LinkedList.h"
+#include "Text_parse.h"
 
 enum definition_status
 {
-    TO_BE_DEFINED = -3,
-    EMPTY = -2,
-    UNDEFINED = -1
+    UNDEFINED = -3,
+    CODE = -2,
+    DATA = -1
 };
 
 typedef struct
 {
-    char *symbol;
+    /* symbol store in node->key */
     int value;
     int base_address;
     int offset;
     int attrib_external;
     int attrib_entry;
-    int attrib_code; /*change to type and add enum -4,-5*/
-    int attrib_data;
+    int label_type; /*data or instruction*/
 } Label;
 
 #endif
 
+void makeEntry(char *label_name, node *label_table);
+int isExtern(char *label_name, node *label_table);
+int isEntry(char *label_name, node *label_table);
+int isDataLabel(char *label_name, node *label_table);
+node *LabelNodeConstructor(char *label_name, int is_extern, int label_type);
+void storeLable(node * LL,node *label); 
+void freeLabel(node *label);
 /*
 create label table with linked list
 collect extern and normal label first
